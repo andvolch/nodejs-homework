1f7035b6b72aeffbcb2b93e9f7d5677da8974e8e
Andrey Volchenskiy <79094272+andvolch@users.noreply.github.com>
2022-01-12 14:38:19 +0200

    finished hw3

---
+++ routes/api/contacts/index.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import {
+  getContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} from "../../../controllers/contacts/index";
+
+import {
+  validateCreate,
+  validateUpdate,
+  validateId,
+  validateUpdateFavorite,
+  validateQuery,
+} from "./validation.js";
+
+const router = new Router();
+
+router.get("/", validateQuery, getContacts);
+
+router.get("/:id", validateId, getContactById);
+
+router.post("/", validateCreate, addContact);
+
+router.delete("/:id", validateId, removeContact);
+
+router.put("/:id", validateId, validateUpdate, updateContact);
+
+router.patch("/:id/favorite", validateUpdateFavorite, updateContact);
+
+export default router;
