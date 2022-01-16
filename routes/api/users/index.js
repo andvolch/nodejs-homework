c9f7cf5c0d09b016ca0485cc71651fe3f815dc1e
Andrey Volchenskiy <79094272+andvolch@users.noreply.github.com>
2022-01-16 16:59:21 +0200

    add uploads

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { aggregetion } from "../../../controllers/users/index";
+import { aggregetion, uploadAvatar } from "../../../controllers/users/index";
 import guard from "../../../middlewares/guard";
+import { upload } from "../../../middlewares/upload";
 import roleAccess from "../../../middlewares/role-access";
 import { Role } from "../../../lib/constants";
 
 const router = new Router();
 
 router.post("/stats/:id", guard, roleAccess(Role.ADMIN), aggregetion);
+router.patch("/avatar", guard, upload.single("avatar"), uploadAvatar);
 
 export default router;
